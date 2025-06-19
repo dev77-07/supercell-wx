c29553bd8934dc14f6e9b0b1cf0e5eeea7864502
AdenKoperczak <awkoperczak@gmail.com>
2025-06-19 11:30:23 -0400

    initial code for rendering derived products. This is very incomplete, but somewhat working

--- scwx-qt/source/scwx/qt/map/map_widget.cpp
+++ scwx-qt/source/scwx/qt/map/map_widget.cpp
@@ -967,7 +967,7 @@ void MapWidget::SelectRadarProduct(common::RadarProductGroup group,
       p->currentTiltIndex_ = 0;
    }
 
-   if (radarProductView == nullptr ||
+   if (radarProductView == nullptr || // TODO
        radarProductView->GetRadarProductGroup() != group ||
        (radarProductView->GetRadarProductGroup() ==
            common::RadarProductGroup::Level2 &&
@@ -1004,7 +1004,9 @@ void MapWidget::SelectRadarProduct(common::RadarProductGroup group,
          const std::string palette =
             (group == common::RadarProductGroup::Level2) ?
                common::GetLevel2Palette(common::GetLevel2Product(productName)) :
-               common::GetLevel3Palette(productCode);
+            (group == common::RadarProductGroup::Level3) ?
+               common::GetLevel3Palette(productCode) :
+               "BV";
 
          auto& paletteSetting =
             settings::PaletteSettings::Instance().palette(palette);
@@ -2136,10 +2138,16 @@ void MapWidgetImpl::RadarProductManagerConnect()
                               radarProductManager_->LoadLevel2Data(latestTime,
                                                                    request);
                            }
-                           else
+                           else if (group == common::RadarProductGroup::Level3)
                            {
                               radarProductManager_->LoadLevel3Data(
                                  product, latestTime, request);
+                           }
+                           else if (group == common::RadarProductGroup::Derived)
+                           {
+                              // TODO
+                              widget_->SelectRadarProduct(
+                                 group, product, 0, latestTime, true);
                            }
                         }
                         catch (const std::exception& ex)
